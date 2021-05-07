d68c5912814b4d8df32316d4e863b894d399545a
yukithis1030 <korethis0816@gmail.com>
2021-05-07 11:23:29 +0900

    [Finish]javascript2

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("Hello World");
+var orange = 100;
+var apple = 200;
+if(orange < apple){
+  alert('みかんはりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごは同じ値段');
+} else {
+  alert('みかんはりんごより高い');
+}
